007a3c36583edfd745a700edfe3f287ecad01d5c
OwnIt Developer <developer@ownit.app>
2026-08-07 22:07:48 +0200

    Migrate build system: catkin → ament_python (ROS 2 Jazzy)
    - package.xml: format 2 → format 3, buildtool rospy/catkin →
      ament_python, all depends updated to rclpy equivalents,
      version bumped to 2.0.0
    - CMakeLists.txt removed (not used by ament_python)
    - setup.py added: entry points for detect_lane, control_lane,
      control_blind; data_files for launch/worlds/models/urdf
    - setup.cfg added: ament_python install destination
    - lane_detector/__init__.py + resource/ marker added (required
      by ament_python package layout)

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+import os
+from glob import glob
+
+package_name = 'lane_detector'
+
+setup(
+    name=package_name,
+    version='2.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        # ament index marker
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        # package.xml
+        ('share/' + package_name, ['package.xml']),
+        # launch files
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')),
+        # worlds
+        (os.path.join('share', package_name, 'worlds'),
+            glob('worlds/*')),
+        # models (recursive)
+        *[
+            (os.path.join('share', package_name, os.path.dirname(f)),
+             [f])
+            for f in glob('models/**/*', recursive=True)
+            if os.path.isfile(f)
+        ],
+        # urdf
+        (os.path.join('share', package_name, 'urdf'),
+            glob('urdf/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Ibrahim Aniasse',
+    maintainer_email='ibrahimaniasse@github.com',
+    description='ROS 2 lane-following for TurtleBot3 on a ramp world',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'detect_lane  = lane_detector.detect_lane:main',
+            'control_lane = lane_detector.control_lane:main',
+            'control_blind = lane_detector.control_blind:main',
+        ],
+    },
+)
